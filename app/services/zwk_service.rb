9ffa6610e2f146a7105d6b5d24b6aa57eaca515a
K.D. Hubbard <hubbard.kd@gmail.com>
2024-03-07 12:46:11 -0700

    refactor be service to allow docker env variable

--- app/services/zwk_service.rb
+++ app/services/zwk_service.rb
@@ -1,4 +1,11 @@
 class ZwkService
+  def backend_url
+    if ENV["docker_backend"] == nil
+      "http://localhost:5000"
+    else
+      ENV["docker_backend"]
+    end
+  end
 
   def search_recipe_name(params)
     get_url("recipes/search?search=#{params}")
@@ -60,28 +67,15 @@ class ZwkService
     end
   end
 
-  # def conn_post
-  #   Faraday.new(url: "https://be-zero-waste-kitchen.onrender.com/api/v1/") do |faraday|
-  #     faraday.headers['Content-Type'] = 'application/json'
-  #     faraday.params['zwk_api_key'] = ENV['ZWK_API_KEY']
-  #   end
-  # end
-
-  # def conn
-  #   Faraday.new(url: "https://be-zero-waste-kitchen.onrender.com/api/v1/") do |faraday|
-  #     faraday.params['zwk_api_key'] = ENV['ZWK_API_KEY']
-  #   end
-  # end
-
   def conn_post
-    Faraday.new(url: "http://localhost:5000/api/v1/") do |faraday|
+    Faraday.new(url: "#{backend_url}/api/v1/") do |faraday|
       faraday.headers['Content-Type'] = 'application/json'
       faraday.params['zwk_api_key'] = ENV['ZWK_API_KEY']
     end
   end
 
   def conn
-    Faraday.new(url: "http://localhost:5000/api/v1/") do |faraday|
+    Faraday.new(url: "#{backend_url}/api/v1/") do |faraday|
       faraday.params['zwk_api_key'] = ENV['ZWK_API_KEY']
     end
   end
